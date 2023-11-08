0e6a0483c09f54af583e5fe8bebedaa0f3733d7b
Matthew Banschbach <matthewbanschbach@pop-os.localdomain>
2023-11-07 19:35:59 -0600

    All Covariance Estimation Files (R)

---
+++ messy_text_process.py
@@ -0,0 +1,14 @@
+in_file = open("covariance_matrix.txt", "r")
+
+out_text = ""
+
+lines = in_file.readlines()
+for line in lines:
+  out_text = out_text + line.replace(" ", ",")
+
+in_file.close()
+
+
+out_file = open("new_covariance_matrix.txt", "w")
+out_file.write(out_text)
+out_file.close()
