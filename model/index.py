797d388474320e0e48ce265dd123f22f6ace61f3
michael-khalfin <michaelk.writing@yahoo.com>
2023-11-21 17:26:05 -0500

    updated class

--- model/index.py
+++ model/index.py
@@ -7,8 +7,27 @@ import gurobipy as gp
 from gurobipy import GRB
 
 class index:
+    """
+    Represents an index and provides methods for creating clusters, 
+    selecting stocks, and valuing the index based on various metrics.
 
-    def __init__(self, period = 0, distance_metric = 0, selection_metric = 0, weight_metric = 0):
+    Parameters:
+    - period (int): The time period for financial data.
+
+    - distance_metric (int): The distance metric used for clustering.
+        - 0: Distance between correlation vectors.
+        - 1: ...
+
+    - num_groups (int): The desired number of clusters.
+
+    - selection_metric (int): The metric used for stock selection.
+        - 0: Capitalization.
+
+    - weight_metric (int): The metric used for weighting the stocks.
+        - 0: Capitalization.
+    """
+
+    def __init__(self, period = 0, distance_metric = 0, num_groups = 5, selection_metric = 0, weight_metric = 0):
         self.period = period
         self.distance_metric = distance_metric
         self.selection_metric = selection_metric
@@ -18,18 +37,16 @@ class index:
         self.largest_indices, self.largest_elems = self.get_k_largest(self.capitalizations[str(self.period)].to_numpy(), 30)
         self.covariance_matrix = self.get_covariance_matrix()
         self.correlation_matrix = self.make_correlation_matrix()
-    
-    def get_covariance_matrix(self):
-        self.covariance_matrix = pd.read_csv(f"data/covariance_matrix{self.period+60}.csv", header=None, encoding='utf-8')
-        self.covariance_matrix = self.covariance_matrix.astype(float)
-        self.covariance_matrix = self.covariance_matrix.to_numpy()
-        self.covariance_matrix = self.covariance_matrix[self.largest_indices]
-        self.covariance_matrix = self.covariance_matrix[:, self.largest_indices]
-
-    def make_correlation_matrix(self):
-        std_devs = np.sqrt(np.diag(self.covariance_matrix))
-        self.correlation_matrix = self.covariance_matrix / np.outer(std_devs, std_devs)
+        self.distance_matrix = self.make_dist_array(distance_metric = distance_metric)
         
+        n = self.distance_matrix.shape[0]
+        self.num_groups = num_groups
+
+        self.env = gp.Env()
+        self.clusters = self.create_clusters(self.distance_matrix, n, num_groups)
+
+        self.stocks = self.select_stocks()
+
     def get_k_largest(self, capitalizations, k):
         """
         Get the indices and corresponding elements of the k largest values in an array.
@@ -50,7 +67,31 @@ class index:
         indices = sorted(indices, reverse=False)
         return indices, largest_elems
     
-    def make_dist_array(self, correlation_matrix):
+    def get_covariance_matrix(self):
+        """
+        Retrieve and process the covariance matrix based on the selected period.
+
+        Returns:
+        - numpy.ndarray: The covariance matrix.
+        """
+        covariance_matrix = pd.read_csv(f"data/covariance_matrix{self.period+60}.csv", header=None, encoding='utf-8')
+        covariance_matrix = self.covariance_matrix.astype(float)
+        covariance_matrix = self.covariance_matrix.to_numpy()
+        covariance_matrix = self.covariance_matrix[self.largest_indices]
+        covariance_matrix = self.covariance_matrix[:, self.largest_indices]
+        return covariance_matrix
+
+    def make_correlation_matrix(self):
+        """
+        Calculate the correlation matrix from the covariance matrix.
+
+        Returns:
+        - numpy.ndarray: The correlation matrix.
+        """
+        std_devs = np.sqrt(np.diag(self.covariance_matrix))
+        return (self.covariance_matrix / np.outer(std_devs, std_devs))
+    
+    def make_dist_array(self, correlation_matrix, distance_metric):
         """
         Transform a correlation matrix to a distance matrix.
 
@@ -61,11 +102,12 @@ class index:
         - numpy.ndarray: The resulting distance matrix.
         """
         distance_matrix = np.zeros_like(correlation_matrix)
-        for i in range(correlation_matrix.shape[0]):
-            for j in range(correlation_matrix.shape[1]):
-                distance_matrix[i, j] = self.distance_bw_vectors(correlation_matrix[i], correlation_matrix[j])
+        if distance_metric == 0:
+            for i in range(correlation_matrix.shape[0]):
+                for j in range(correlation_matrix.shape[1]):
+                    distance_matrix[i, j] = self.distance_bw_vectors(correlation_matrix[i], correlation_matrix[j])
         return distance_matrix
-
+    
     def distance_bw_vectors(self, v1, v2):
         """
         Compute the distance between two vectors.
@@ -139,3 +181,22 @@ class index:
         else:
             print("No feasible solution found.")
             return []
+    
+    def select_stocks(self, selection_metric):
+        """
+        Select stocks based on the chosen metric within the created clusters.
+
+        Parameters:
+        - selection_metric (int): The metric used for stock selection.
+
+        Returns:
+        - List[int]: A list of selected stocks.
+        """
+        kstocks = []
+        if selection_metric == 0:
+            for cluster in self.clusters:
+                caps = []
+                caps = [self.largest_elems[i] for i in cluster]
+                i = np.where(self.largest_elems == max(caps))
+                kstocks.append(i[0][0])
+        return kstocks
